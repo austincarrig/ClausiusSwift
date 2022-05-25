f218930b07737f33c9db3e8bf6f3150d6f80ed56
Austin Carrig <austincarrig@gmail.com>
2022-05-25 15:53:09 -0400

    Added empty unit test file for H2OWagnerPruss

---
+++ ClausiusSwiftTests/H2OWagnerPrussTests.swift
@@ -0,0 +1,20 @@
+//
+//  H2OWagnerPrussTests.swift
+//  ClausiusSwiftTests
+//
+//  Created by Austin Carrig on 5/25/22.
+//
+
+import XCTest
+
+class H2OWagnerPrussTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+}
